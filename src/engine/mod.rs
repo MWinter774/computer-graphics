f30397bc40eb36b5deff50a3e3fb434811a549c5
Maor Winter <winter.maor@gmail.com>
2025-03-19 20:59:25 +0200

    Implemented input-system

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,9 +1,11 @@
 pub use self::engine::Engine;
 use self::engine_context::EngineContext;
 use self::glfw_context::GLFWContext;
+use self::input_system::InputSystem;
 use self::window::Window;
 
 pub mod engine;
 mod engine_context;
 mod glfw_context;
+mod input_system;
 mod window;
