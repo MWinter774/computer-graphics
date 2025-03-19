753c94e5db1b90b622f0b2de54638a705acedcce
Maor Winter <winter.maor@gmail.com>
2025-03-19 20:54:27 +0200

    Basic window creation

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -7,9 +7,29 @@ pub struct Engine {
 impl Engine {
     pub fn new() -> Self {
         Self {
-            context: engine::EngineContext::new(),
+            context: engine::EngineContext::new("Engine", 800, 600),
         }
     }
 
-    pub fn run(&self) {}
+    pub fn run(&mut self) {
+        let mut window_should_close = false;
+
+        self.context.initialize();
+
+        unsafe {
+            gl::Enable(gl::DEPTH_TEST);
+            gl::DepthFunc(gl::LESS);
+        }
+
+        while !window_should_close {
+            // Swap the buffers of the opengl window and updates event pipeline
+            let frame_data = self.context.next_frame();
+
+            unsafe {
+                gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+                gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+            }
+        }
+        self.context.set_should_terminate(true);
+    }
 }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,5 +1,9 @@
 pub use self::engine::Engine;
 use self::engine_context::EngineContext;
+use self::glfw_context::GLFWContext;
+use self::window::Window;
 
 pub mod engine;
 mod engine_context;
+mod glfw_context;
+mod window;
